333daeb2a071538a82498efb1409a120905137ab
Joseph Perrino <107595901+sei-jperrino@users.noreply.github.com>
2022-09-13 15:27:37 -0400

    UI information adjustments
    - Time tooltip can show seconds, and does show seconds on ticket details
    - Comment button is grayed out initially in ticket details and moved below attachments pane

--- projects/gameboard-ui/src/app/utility/pipes/short-time.pipe.ts
+++ projects/gameboard-ui/src/app/utility/pipes/short-time.pipe.ts
@@ -6,10 +6,10 @@ export class ShortTimePipe implements PipeTransform {
 
   constructor(private config: ConfigService) {}
 
-  transform(date: any, timeZone: boolean = false): string {
+  transform(date: any, timeZone: boolean = false, includeSeconds: boolean = false): string {
     const style = timeZone
-      ? this.config.shorttz_formatter
-      : this.config.shortdate_formatter
+      ? (includeSeconds ? this.config._shorttz_formatter_with_seconds : this.config.shorttz_formatter)
+      : (includeSeconds ? this.config._shortdate_formatter_with_seconds : this.config.shortdate_formatter)
     ;
     return style.format(new Date(date));
   }
