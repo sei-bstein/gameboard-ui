c449db7841849f1c4759e7d6a41e0dbb0c7c45ea
Ben Stein <bsstein@sei.cmu.edu>
2022-10-21 01:23:45 -0400

    Fun with CSS for morphing text

--- projects/gameboard-ui/src/app/morphing-text/morphing-text.component.ts
+++ projects/gameboard-ui/src/app/morphing-text/morphing-text.component.ts
@@ -1,62 +1,53 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-morphing-text',
   templateUrl: './morphing-text.component.html',
   styleUrls: ['./morphing-text.component.scss']
 })
-export class MorphingTextComponent implements OnInit, AfterViewInit {
+export class MorphingTextComponent implements AfterViewInit {
+  @Input() cooldownTime = 4;
+  @Input() morphTime = 2;
+  @Input() messages: string[] = [
+    "Starting preflight checks...",
+    "Navigation systems: ONLINE.",
+    "Propulsion systems: ONLINE.",
+    "Fuel reserves: NOMINAL.",
+    "Weapons systems: GREEN.",
+    "Hardlight shields: GREEN.",
+    "Cargo weight: WNL.",
+    "ALL SYSTEMS: GREEN.",
+    "Daunless, you are cleared for departure."
+  ];
+  @Input() isRandom = false;
+
   @ViewChild("text1") text1!: ElementRef<HTMLSpanElement>;
   @ViewChild("text2") text2!: ElementRef<HTMLSpanElement>;
 
-  private readonly MORPH_TIME = 1;
-  private readonly COOLDOWN_TIME = 0.25;
-  private readonly START_TIME = new Date();
-
-  private _cooldown = this.COOLDOWN_TIME;
+  private _cooldown = this.cooldownTime;
   private _morph = 0;
   private _referenceTime = new Date();
-  private _textIndex = 0;
-
-  private entries = [
-    "If",
-    "You",
-    "Like",
-    "It",
-    "Please",
-    "Give",
-    "a Love",
-    ":)",
-    "by @DotOnion"
-  ];
-
-  constructor() { }
+  private _textIndex = this.messages.length - 1;
 
   ngAfterViewInit(): void {
-    this.updateTextIndex(0);
-    
-    
+    this.updateTextContent(this._textIndex % this.messages.length, (this._textIndex + 1) % this.messages.length);
     this.animate();
   }
 
-  ngOnInit(): void {
-
-  }
-
-  private doMorph() {
+  private doMorph(nextIndex: number) {
     this._morph -= this._cooldown;
     this._cooldown = 0;
 
-    let fraction = this._morph / this.MORPH_TIME;
+    let fraction = this._morph / this.morphTime;
     if (fraction > 1) {
-      this._cooldown = this.COOLDOWN_TIME;
+      this._cooldown = this.cooldownTime;
       fraction = 1;
     }
 
-    this.setMorph(fraction);
+    this.setMorph(fraction, nextIndex);
   }
 
-  private setMorph(fraction: number) {
+  private setMorph(fraction: number, nextIndex: number) {
     this.text2.nativeElement.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
     this.text2.nativeElement.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
@@ -64,12 +55,12 @@ export class MorphingTextComponent implements OnInit, AfterViewInit {
     this.text1.nativeElement.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
     this.text1.nativeElement.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
-    this.updateTextIndex(this._textIndex);
+    this.updateTextContent(this._textIndex, nextIndex);
   }
 
-  private updateTextIndex(index: number) {
-    this.text1.nativeElement.textContent = this.entries[this._textIndex % this.entries.length];
-    this.text1.nativeElement.textContent = this.entries[(this._textIndex + 1) % this.entries.length];
+  private updateTextContent(index: number, nextIndex: number) {
+    this.text1.nativeElement.textContent = this.messages[index % this.messages.length];
+    this.text2.nativeElement.textContent = this.messages[nextIndex % this.messages.length];
   }
 
   private doCooldown() {
@@ -82,22 +73,30 @@ export class MorphingTextComponent implements OnInit, AfterViewInit {
     this.text1.nativeElement.style.opacity = "0%";
   }
 
-  private animate() {
-    requestAnimationFrame(this.animate);
+  private resolveNextIndex(currentIndex: number) {
+    if (!this.isRandom) {
+      return currentIndex + 1 % this.messages.length;
+    }
 
-    const shouldIncrementIndex = this._cooldown > 0;
+    const random = Math.floor(Math.random() * (this.messages.length));
+    return random === currentIndex ? random - 1 : random;
+  }
+
+  private animate(timestamp?: DOMHighResTimeStamp) {
+    requestAnimationFrame(this.animate.bind(this));
+
+    const shouldAdvanceIndex = this._cooldown > 0;
     const newTime = new Date();
-    const dt = (new Date().getUTCDate() - this._referenceTime.getUTCDate()) / 1000;
+    const dt = (newTime.getTime() - this._referenceTime.getTime()) / 1000;
     this._referenceTime = newTime;
-
     this._cooldown -= dt;
 
     if (this._cooldown <= 0) {
-      if (shouldIncrementIndex) {
-        this._textIndex++;
+      if (shouldAdvanceIndex) {
+        this._textIndex = this.resolveNextIndex(this._textIndex);  
       }
 
-      this.doMorph();
+      this.doMorph(this._textIndex);
     }
     else {
       this.doCooldown();
